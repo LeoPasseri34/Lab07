9ed6a8d1fd53fd7aa62c0c20cd96a7d3fa94b89f
lpass <s310842@studenti.polito.it>
2025-04-14 16:12:52 +0200

    risolto problema mesi giugno e gennaio

--- UI/controller.py
+++ UI/controller.py
@@ -1,7 +1,7 @@
 import flet as ft
 
 from UI.view import View
-from model.model import Model
+from model.modello import Model
 
 
 class Controller:
@@ -14,13 +14,34 @@ class Controller:
         self._mese = 0
 
     def handle_umidita_media(self, e):
-        pass
-
+        mese = self._view.dd_mese.value
+        if (mese==""):
+            self._view.create_alert("Attenzione, selezionare un mese!")
+            self._view.update_page()
+            return
+        else:
+            stats_umidita = self._model.get_umidita_media(mese)
+            for r in stats_umidita:
+                localita = r["Localita"]
+                media = round(r["umidita_media"], 4)  # Arrotonda a 4 cifre decimali
+                self._view.lst_result.controls.append(ft.Text(f"{localita}: {media}"))
+                self._view.update_page()
 
 
     def handle_sequenza(self, e):
-        pass
+        mese = self._view.dd_mese.value
+        if mese == "":
+            self._view.create_alert("Attenzione, selezionare un mese!")
+            return
+        sequenza, costo = self._model.calcola_sequenza(mese)
+        print(sequenza)
+        self._view.lst_result.controls.clear()
+        self._view.lst_result.controls.append(ft.Text(f"Il costo della sequenza è {costo}"))
+        for i in sequenza:
+            self._view.lst_result.controls.append(ft.Text(i))
+        self._view.update_page()
 
     def read_mese(self, e):
         self._mese = int(e.control.value)
+
 
--- database/meteo_dao.py
+++ database/meteo_dao.py
@@ -5,7 +5,7 @@ from model.situazione import Situazione
 class MeteoDao():
 
     @staticmethod
-    def get_all_situazioni():
+    def get_all_situazioni(mese):
         cnx = DBConnect.get_connection()
         result = []
         if cnx is None:
@@ -13,9 +13,9 @@ class MeteoDao():
         else:
             cursor = cnx.cursor(dictionary=True)
             query = """SELECT s.Localita, s.Data, s.Umidita
-                        FROM situazione s 
+                        FROM situazione s
                         ORDER BY s.Data ASC"""
-            cursor.execute(query)
+            cursor.execute(query, (mese,))
             for row in cursor:
                 result.append(Situazione(row["Localita"],
                                          row["Data"],
@@ -24,4 +24,50 @@ class MeteoDao():
             cnx.close()
         return result
 
+    @staticmethod
+    def get_umidita(mese):
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("Connessione fallita")
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            query = """SELECT s.Localita, avg(s.Umidita) as umidita_media
+                        FROM situazione s 
+                        where month(s.`Data`) = %s
+                        group by s.Localita """
+            cursor.execute(query, (mese,))
+            for row in cursor:
+                result.append(row)
+            cursor.close()
+            cnx.close()
+        return result
+
+    @staticmethod
+    def get_situazioni_mese(mese):
+        cnx = DBConnect.get_connection()
+        result = []
+        if cnx is None:
+            print("Connessione fallita")
+        else:
+            cursor = cnx.cursor(dictionary=True)
+            query = """SELECT s.Localita, s.Data, s.Umidita
+                            FROM situazione s 
+                            WHERE MONTH(s.Data)=%s
+                            AND DAY(s.Data)<=15
+                            ORDER BY s.Data ASC"""
+            cursor.execute(query, (mese,))
+            for row in cursor:
+                result.append(Situazione(row["Localita"],
+                                         row["Data"],
+                                         row["Umidita"]))
+            cursor.close()
+            cnx.close()
+        return result
+
+
+if __name__ == '__main__':
+    meteo = MeteoDao()
+    print(meteo.get_situazioni_mese('2'))
+    print(meteo.get_umidita('4'))
 
